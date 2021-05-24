914b74545d2c33a0b36f2d2500e72e1233b48f2b
Batoolayyad <batoolayyad1996@gmail.com>
2021-05-24 18:36:46 +0300

    second push for files

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express=require ('express');
+const weatherData=require('./data/weather.json')
+const cors=require('cors');
+require('dotenv').config();
+
+const server=express();
+server.use(cors());
+
+const PORT = process.env.PORT;
+
+// http://localhost:3001/getLocation?city_name=Amman
+server.get('/weather',(req,res)=>{
+    let cityData = req.query.city_name;
+    let searchQuery = weatherData.find(item=>{
+        if (item.city_name == cityData)
+        return item;
+    })
+    res.send(searchQuery)
+})
+server.listen(PORT,()=>{
+    console.log(`Listening on PORT ${PORT}`);
+})
+
